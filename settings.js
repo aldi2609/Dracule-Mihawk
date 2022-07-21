376b28d179434b7984c587403734b7eb6000b985
aldi2609 <109309282+aldi2609@users.noreply.github.com>
2022-07-21 23:10:43 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,14 +12,14 @@ const chalk = require('chalk')
 global.zenzkey = 'Your_Apikey' //Register Atau Beli Di zenzapis.xyz
 
 //===> SESUAIKAN DENGAN KEBUTUHAN MU
-global.ownerNumber = ['6285609233482','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
-global.Rowner = '6285609233482' //Owner Utama
-global.ownerName = 'Nzrl Afndi' //Ganti Jadi Namamu
-global.packname = 'Dracule Mihawk' //Ganti Jadi Nama Botmu
-global.author = 'By NzrlAfndi' //Ganti Jadi Namamu
+global.ownerNumber = ['628999625005','628999625005'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
+global.Rowner = '628999625005' //Owner Utama
+global.ownerName = 'Aldi' //Ganti Jadi Namamu
+global.packname = 'Aldi Bot' //Ganti Jadi Nama Botmu
+global.author = 'By Aldi' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi' //Saran Gausah Diganti Lagi
-global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
+global.webme = 'https://chat.whatsapp.com/CVxGqm1TBnRAyqkH4eYGWr' //Ganti Jadi Bebas Web Sosmed Mu
 
 //====> SARAN GAUSAH DIGANTI SIH DAH PAS SEMUA
 global.limitAwal = {
